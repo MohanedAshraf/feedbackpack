f3a5a8e11b7c1c77ea52393e3b64c1a376664b22
mohanedashraf <mohaned.ashraf.elsayed@gmail.com>
2021-12-11 10:09:36 +0200

    :sparkles: make passport aware of cookies
    make passport aware of cookies

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require('express')
 const mongoose = require('mongoose')
-require('./models/User')
-require('./services/passport')
-const {mongoURI} = require('./config/keys')
-
-
+const passport = require('passport')
+const cookieSession = require('cookie-session')
+const {mongoURI , cookieKey} = require('./config/keys')
 const connectDB = async () => {
     const conn = await mongoose.connect(mongoURI)
     console.log(`MongoDB Connected: ${conn.connection.host}`)
 }
+
 connectDB()
+require('./models/User')
+require('./services/passport')
 
 const app = express()
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [cookieKey]
+    })
+)
+app.use(passport.initialize())
+app.use(passport.session())
 
 require('./routes/auth')(app)
 
