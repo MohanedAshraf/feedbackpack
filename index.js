f781f36a412280ccd9eeecb740fee4997eb17099
mohanedashraf <mohaned.ashraf.elsayed@gmail.com>
2021-12-30 10:42:49 +0200

    :art: add eslintto backend

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const mongoose = require('mongoose')
 const passport = require('passport')
 const cookieSession = require('cookie-session')
-const {mongoURI , cookieKey} = require('./config/keys')
+const { mongoURI, cookieKey } = require('./config/keys')
 const connectDB = async () => {
-    const conn = await mongoose.connect(mongoURI)
-    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  const conn = await mongoose.connect(mongoURI)
+  console.log(`MongoDB Connected: ${conn.connection.host}`)
 }
 
 connectDB()
@@ -15,19 +15,18 @@ require('./services/passport')
 const app = express()
 
 app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: [cookieKey]
-    })
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [cookieKey]
+  })
 )
 app.use(passport.initialize())
 app.use(passport.session())
 
 require('./routes/auth')(app)
 
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT , (err) => {
-    if(err) console.log(err);
-    else console.log("Server is Running on Port:" , PORT);
+const PORT = process.env.PORT || 5000
+app.listen(PORT, (err) => {
+  if (err) console.log(err)
+  else console.log('Server is Running on Port:', PORT)
 })
--- services/passport.js
+++ services/passport.js
@@ -1,41 +1,31 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const mongoose = require('mongoose')
-const {googleClientID , googleClientSecret} = require ('../config/keys')
+const { googleClientID, googleClientSecret } = require('../config/keys')
 
 const User = mongoose.model('users')
 
-passport.serializeUser((user , done )=> {
-    done(null , user.id)
-
+passport.serializeUser((user, done) => {
+  done(null, user.id)
 })
 
-
-passport.deserializeUser((id , done )=> {
-    User.findById(id).then(user => {
-        done(null , user)
-    })
-
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user)
+  })
 })
-
 
 passport.use(new GoogleStrategy({
-    clientID: googleClientID,
-    clientSecret: googleClientSecret,
-    callbackURL: '/auth/google/callback',
-    proxy: true
-} , async (accessToken , refreshToken , profile  , done) => {
+  clientID: googleClientID,
+  clientSecret: googleClientSecret,
+  callbackURL: '/auth/google/callback',
+  proxy: true
+}, async (accessToken, refreshToken, profile, done) => {
+  const user = await User.findOne({ googleId: profile.id })
+  if (user) {
+    done(null, user)
+  }
 
-   const user = await User.findOne({ googleId: profile.id})
-   if(user){
-    done(null , user)
-   }
-   
-   const newUser = await new User({ googleId: profile.id}).save()
-    done(null , newUser)
-   
-  
- 
-
-
+  const newUser = await new User({ googleId: profile.id }).save()
+  done(null, newUser)
 }))
