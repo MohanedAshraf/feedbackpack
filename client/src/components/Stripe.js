4ead40af1818712d1cc8081cbf96f7d0643353e9
mohanedashraf <mohaned.ashraf.elsayed@gmail.com>
2021-12-30 10:32:06 +0200

    :boom: foundation of stripe payement

---
+++ client/src/components/Stripe.js
@@ -0,0 +1,13 @@
+import {Component} from 'react'
+import StripeCheckout from 'react-stripe-checkout'
+
+class Stripe extends Component {
+
+    render(){
+        return(
+            <StripeCheckout/> 
+        );
+    }
+} 
+
+export default Stripe;
