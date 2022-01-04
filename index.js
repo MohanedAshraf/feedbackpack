f77143db0341160abc4bdaf64fbfaf9f6779c0f6
mohanedashraf <mohaned.ashraf.elsayed@gmail.com>
2022-01-04 12:36:13 +0200

    :rocket: add static filies to express and a wildcard route

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const mongoose = require('mongoose')
 const passport = require('passport')
 const cookieSession = require('cookie-session')
@@ -27,6 +28,10 @@ app.use(passport.session())
 require('./routes/auth')(app)
 require('./routes/billing')(app)
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (_, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 const PORT = process.env.PORT || 5000
 app.listen(PORT, (err) => {
   if (err) console.log(err)
